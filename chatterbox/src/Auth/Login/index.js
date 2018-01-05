16c6f653b8b0ac3c71c869aa119347d938446f7e
MahlekenyaneTseole <stseole@retrorabbit.co.za>
2018-01-05 11:15:40 +0200

    init

---
+++ chatterbox/src/Auth/Login/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        Login stuff here
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ chatterbox/src/Auth/Register/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+      <div className="Register">
+        Register stuff here
+      </div>
+    );
+  }
+}
+
+export default Register;
