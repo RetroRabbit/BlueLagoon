532356023c47421cb8163edd111ad3d4e580802c
Dawid Eygelaar <deygelaar@retrorabbit.co.za>
2018-01-05 13:33:31 +0200

    sidebar changes

--- chatterbox/src/Main/MainArea/ChattingComponent/Sidebar/index.js
+++ chatterbox/src/Main/MainArea/ChattingComponent/Sidebar/index.js
@@ -15,8 +15,8 @@ class Sidebar extends React.Component {
                         <img src ={logo} alt ="logo" className="oval-3-copy"/>  
                     </div>
                     <div className="the-practice-of-ciga">
-                            The practice of cigar smoking has been on the rise in the U.S. since the early 90’s. In part due to a reputation as a glamorous alternative to cigarette smoking, the increase can also be tied to its popularity among celebrities, as well as to the social nature of its practice. But whatever the reason, it is clear that cigars are big business in the U.S. with higher sales of premium brands each and every year.
-                            Magazines like Cigar Aficionado portray cigar smoking as alluring, and perhaps slightly risqu     
+                            <p>The practice of cigar smoking has been on the rise in the U.S. since the early 90’s. In part due to a reputation as a glamorous alternative to cigarette smoking, the increase can also be tied to its popularity among celebrities, as well as to the social nature of its practice. But whatever the reason, it is clear that cigars are big business in the U.S. with higher sales of premium brands each and every year.
+                            Magazines like Cigar Aficionado portray cigar smoking as alluring, and perhaps slightly risqu   </p>  
                     </div>
                 </div>
             </div>
