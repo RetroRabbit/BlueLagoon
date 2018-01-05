1f5aaa5806fbbe9c71411beea27b5dd6a807a9b9
CeeyarRbt <cndovela@retrorabbit.co.za>
2018-01-05 16:40:25 +0200

    Nothing much, just Added two buttons and a dropdown

--- chatterbox/src/App.js
+++ chatterbox/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'; 
 import './App.css';
 import Header from './Main/Header';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <Header/>
+  </MuiThemeProvider>
     );
   }
 }
--- chatterbox/src/Main/Header/index.js
+++ chatterbox/src/Main/Header/index.js
@@ -1,30 +1,101 @@
 import React, { Component } from 'react';
+import { Manager, Target, Popper } from 'react-popper';
 import './index.css';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Button from 'material-ui/Button';
+import { MenuItem, MenuList } from 'material-ui/Menu';
+import Grow from 'material-ui/transitions/Grow';
+import Paper from 'material-ui/Paper';
+import { withStyles } from 'material-ui/styles';
+import ClickAwayListener from 'material-ui/utils/ClickAwayListener';
 
 function handleClick() {
   alert('onClick triggered on the title component');
 }
 
-const styles = {
-  title: {
-    cursor: 'pointer',
+const style = {
+  margin: 12,
+  root: {
+    display: 'flex',
+  },
+  popperClose: {
+    pointerEvents: 'none',
   },
 };
 
-/**
- * This example uses an [IconButton](/#/components/icon-button) on the left, has a clickable `title`
- * through the `onClick` property, and a [FlatButton](/#/components/flat-button) on the right.
- */
-const Header = () => (
-    <AppBar
-    title={<span style={styles.title}>Title</span>}
-    onTitleClick={handleClick}
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    iconElementRight={<FlatButton label="Save" />}
-  />
-);
-export default Header;
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClick = () => {
+    if(this.state.open) {
+      this.setState({ open: false });
+    }
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  render() {
+    return (
+      <div  > 
+      <AppBar  
+      iconElementLeft={
+        <div>
+        <RaisedButton label="New Chat" primary={true} style={style} />
+        <RaisedButton label="New Group" primary={true} style={style} />
+      </div>
+    }
+      iconElementRight={
+      <div> 
+      <Manager>
+          <Target>
+            <Button
+              aria-owns={this.state.open ? 'menu-list' : null}
+              aria-haspopup="true"
+              onClick={this.handleClick}
+            >
+          Addie Hogan
+            </Button>
+          </Target>
+          <Popper
+            placement="bottom-start"
+            eventsEnabled={"open"}
+            
+          >
+          {(this.state.open) ?
+            <ClickAwayListener onClickAway={this.handleClose}>
+              <Grow in={"open"} id="menu-list" style={{ transformOrigin: '0 0 0' }}>
+                <Paper>
+                  <MenuList role="menu">
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                  </MenuList>
+                </Paper>
+              </Grow>
+            </ClickAwayListener> : null}
+          </Popper>
+        </Manager>
+        </div>
+        }
+    />
+      </div>
+      
+    );
+  }
+}
+
+export default Header;  
