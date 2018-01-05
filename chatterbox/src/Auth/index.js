620db0c0746131989c49bb23874e371fa7ee9b34
Mahlekenyane <mahtseole@gmail.com>
2018-01-05 10:35:27 +0200

    chatterbox init

---
+++ chatterbox/src/Auth/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './Auth.css';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div className="Auth">
+        <p className="auth-box">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Auth;
