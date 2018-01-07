f4b9d551278628cba320ffdb08faf4ef7d856548
MahlekenyaneTseole <stseole@retrorabbit.co.za>
2018-01-07 14:58:09 +0200

    Chat component done

--- chatterbox/src/Main/index.js
+++ chatterbox/src/Main/index.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import '../App.css';
+import './Main.css';
+
+import ChatArea from "./MainArea/ChattingComponent/ChatArea"
 
 class MainArea extends Component {
   render() {
+    console.log("Main",this.props);
     return (
-      <div className="App">
+      <div className="MainApp">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChatArea/>
       </div>
     );
   }
