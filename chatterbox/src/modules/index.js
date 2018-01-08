288b2b4ffe485395c0c4c0618b84535162e20028
hvandermerwe <hvandermerwe@retrorabbit.co.za>
2018-01-08 14:23:40 +0200

    added root reducer

---
+++ chatterbox/src/modules/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import UserReducer from './settings/user-reducer.js'
+import EditUserReducer from './settings/edit-user-reducer.js'
+
+const rootReducer = combineReducers({
+  user: UserReducer,
+  edituser: EditUserReducer
+});
+
+export default rootReducer;
