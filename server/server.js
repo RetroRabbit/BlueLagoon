6c46846c63c550cb702fb442b50c66e4d397a0b2
MahlekenyaneTseole <stseole@retrorabbit.co.za>
2018-01-08 14:24:30 +0200

    Server init

---
+++ server/server.js
@@ -0,0 +1,69 @@
+var http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    errorhandler = require('errorhandler');
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+var app = express();
+
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+// app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+app.use(require('./routes'));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+// finally, let's start our server...
+var server = app.listen( process.env.PORT || 8088, '0.0.0.0', function(){
+  console.log('Listening on port ' + server.address().port);
+});
+
+//init chat system
+// require('./public/livechats').chatsInit(server);
