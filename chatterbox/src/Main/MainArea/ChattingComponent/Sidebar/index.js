8ce6b39b0e3417d4cabefee7fb0d8c7c27477799
CeeyarRbt <cndovela@retrorabbit.co.za>
2018-01-09 12:51:35 +0200

    fixed issues after merge

--- chatterbox/src/Main/MainArea/ChattingComponent/Sidebar/index.js
+++ chatterbox/src/Main/MainArea/ChattingComponent/Sidebar/index.js
@@ -5,7 +5,7 @@ import logo1 from '../../../../assets/male2.png';
 import logo2 from '../../../../assets/female1.png';
 import logo3 from '../../../../assets/Male3.png';
 import slogo from '../../../../assets/search.png';
-import UserChat from './UserChat';
+import UserChat from './userchat';
 
 class Sidebar extends React.Component {
     constructor(props) {
