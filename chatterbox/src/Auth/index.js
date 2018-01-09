eab63131bc977bbd3deeda08250e61e5c9496b56
MahlekenyaneTseole <stseole@retrorabbit.co.za>
2018-01-09 10:02:53 +0200

    merge develop origin/develop

--- chatterbox/src/Auth/index.js
+++ chatterbox/src/Auth/index.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 import Register from './Register';
 import { Route } from 'react-router-dom'
 import Login from "./Login"
-
 import './Auth.css';
 
-class Auth extends Component {
-	constructor(e){
-		super(e);
-	}
 
-  render() {
-    return (
-      <div className="Auth">
-      	<div className="auth-box">
-      		<Route path={`${this.props.match.path}/login`} component={Login} />
-      		<Route path={`${this.props.match.path}/register`} component={Register} />
-	    </div>
-      </div>
-    );
-  }
+class Auth extends Component {
+
+
+
+    constructor(e) {
+        super(e);
+    }
+
+
+    render() {
+        return (
+            <div className="Auth">
+                <div className="auth-box">
+
+                    <Route path={`${this.props.match.path}/login`} component={Login} />
+                    <Route path={`${this.props.match.path}/register`} component={Register} />
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Auth;
--- chatterbox/src/Main/index.js
+++ chatterbox/src/Main/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './Main.css';
-
 import MainArea from "./MainArea"
 
 class Main extends Component {
