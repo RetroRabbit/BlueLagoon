e4e4234d65953b181c3224081d3091919b4d6aed
hvandermerwe <hvandermerwe@retrorabbit.co.za>
2018-01-10 07:45:54 +0200

    fixed register reducer declaration in the root reducer

--- chatterbox/src/modules/index.js
+++ chatterbox/src/modules/index.js
@@ -11,7 +11,7 @@ export default combineReducers({
     sidebar:sidebar,
     user: userReducer,
     settings: userSettings,
-    header:header
-     register,
+    header:header,
+    register:register,
     settings: userSettings
 });
