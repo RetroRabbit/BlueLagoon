21d7b31007d5cb77dfec692f1af516a157bcda93
hvandermerwe <hvandermerwe@retrorabbit.co.za>
2018-01-10 07:48:18 +0200

    refactored edit-user-reducer.js

--- chatterbox/src/modules/settings/edit-user-reducer.js
+++ chatterbox/src/modules/settings/edit-user-reducer.js
@@ -1,13 +1,13 @@
-export const EDIT_USER_NAME = 'edit-user-reducer/EDIT_USER_NAME_REQUESTED'
-export const EDIT_USER_EMAIL = 'edit-user-reducer/EDIT_USER_EMAIL_REQUESTED'
-export const SAVE_CHANGES_USER_DETAILS = 'edit-user-reducer/EDIT_USER_PROFILE_PICTURE_REQUESTED'
-export const EDIT_PROFILE_PICTURE = 'edit-user-reducer/EDIT_USER_PROFILE_PICTURE_REQUESTED'
-
 const initialState = {
-    editUserName: false,
-    editUserEmail: false,
-    editProfilePicture: false,
-    saveChanges: false
+    user: {
+        name: "Rian van der Merwe",
+        email: 'hvandermerwe@retrorabbit.co.za',
+        profilePic: 'http://www.legobatman.com/assets/media/global/header/batwink-loop.gif'
+    },
+    editName: false,
+    editEmail: false,
+    editProfilePic: false,
+    changesMade: false
 }
 
 export default function(state=initialState, action) {
@@ -15,67 +15,37 @@ export default function(state=initialState, action) {
         case 'EDIT_USER_NAME':
            return {
                ...state,
-               editUserName: !state.editUserName
+               editName: !state.editName,
+               editEmail: false,
+               editProfilePic: false
             }
  
         case 'EDIT_USER_EMAIL':
-            return{
+            return {
                 ...state,
-                editUserEmail: !state.editUserEmail,
-            }
-
-        case 'SAVE_CHANGES_USER_DETAILS':
-            return{
-                ...state,
-                saveChanges: !state.saveChanges,
-                editUserName: !state.editUserName,
-                editUserEmail: !state.editUserName,
-                editProfilePicture: !state.editUserName
+                editEmail: !state.editEmail,
+                editName: false,
+                editProfilePic: false
             }
 
         case 'EDIT_PROFILE_PICTURE':
-            return{
+            return {
                 ...state,
-                editProfilePicture: true
+                editProfilePic: !state.editProfilePic,
+                editName: false,
+                editEmail: false,
             }
+
+        case 'SAVE_CHANGES_USER_DETAILS':
+            return {
+                ...state,
+                editName: false,
+                editEmail: false,
+                editProfilePic: false,
+                changesMade: true
+        }
 
         default:
             return state;
-    }
-}
-
-export const editUserNameRequest = (action) => {
-    console.log('edit user ' + action.firstname + ' ' + action.lastname);
-    return dispatch  => {
-        dispatch({
-            type: "EDIT_USER_NAME_REQUESTED"
-        })
-    }
-}
-
-export const editUserEmailRequest = (action) => {
-    console.log('edit user email: ' + action.email + " ");
-    return dispatch  => {
-        dispatch({
-            type: "EDIT_USER_EMAIL_REQUESTED"
-        })
-    }
-}
-
-export const editUserProfilePictureRequest = (action) => {
-    console.log('edit profile picture of ' +  action.firstname);
-    return dispatch  => {
-        dispatch({
-            type: "EDIT_USER_PROFILE_PICTURE_REQUESTED"
-        })
-    }
-}
-
-export const saveChangesRequest = (action) => {
-    console.log('saving changes to ' + action.firstname);
-    return dispatch  => {
-        dispatch({
-            type: "SAVE_CHANGES_REQUESTED"
-        })
     }
 }
