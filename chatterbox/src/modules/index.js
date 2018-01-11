d76379c5e8ee318ad25b5cc80e61aab7116badb8
CeeyarRbt <cndovela@retrorabbit.co.za>
2018-01-11 15:38:42 +0200

    redux conflict

--- chatterbox/src/modules/index.js
+++ chatterbox/src/modules/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import sidebar from './Sidebar';
 import header from './Header';
-import login from "./login/login-reducer"
+import login from './login/login-reducer';
+
 import EditUserReducer from '../modules/settings/edit-user-reducer'
 import userReducer from '../modules/settings/user-reducer'
 import register from "./register"
